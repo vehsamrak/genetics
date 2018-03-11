801ba892f33714bf6a98bbedc1104e34867734a0
Petr Karmashev <smonkl@bk.ru>
2018-03-11 23:23:41 +0300

    Reformatted game.go with gofmt

--- game.go
+++ game.go
@@ -1,71 +1,71 @@
 package main
 
 import (
-    "math/rand"
-    "time"
+	"math/rand"
+	"time"
 )
 
 const CELL_TYPE_FOOD = 1
 const CELL_TYPE_OBSTACLE = 2
 
 type Game struct {
-    X         int
-    Y         int
-    field     [][]int
-    Seed      int64
-    FreeSpace int
+	X         int
+	Y         int
+	field     [][]int
+	Seed      int64
+	FreeSpace int
 }
 
 func init() {
-    rand.Seed(time.Now().Unix())
+	rand.Seed(time.Now().Unix())
 }
 
 func (game *Game) CreateField() {
-    if game.Seed != 0 {
-        rand.Seed(game.Seed)
-    }
+	if game.Seed != 0 {
+		rand.Seed(game.Seed)
+	}
 
-    if game.FreeSpace == 0 {
-        game.FreeSpace = (game.X + game.Y) / 2
-    }
+	if game.FreeSpace == 0 {
+		game.FreeSpace = (game.X + game.Y) / 2
+	}
 
-    game.field = make([][]int, game.X)
+	game.field = make([][]int, game.X)
 
-    for x := 0; x < game.X; x++ {
-        var gameFieldY []int
-        gameFieldY = make([]int, game.Y)
+	for x := 0; x < game.X; x++ {
+		var gameFieldY []int
+		gameFieldY = make([]int, game.Y)
 
-        for y := 0; y < game.Y; y++ {
-            gameFieldY[y] = rand.Intn(game.FreeSpace)
-        }
+		for y := 0; y < game.Y; y++ {
+			gameFieldY[y] = rand.Intn(game.FreeSpace)
+		}
 
-        game.field[x] = gameFieldY
-    }
+		game.field[x] = gameFieldY
+	}
 }
 
 func (game *Game) ViewAsText() (output string) {
-    for _, row := range game.field {
-        for _, cell := range row {
-            output += game.transformCellToSymbol(cell)
-        }
+	for _, row := range game.field {
+		for _, cell := range row {
+			output += game.transformCellToSymbol(cell)
+		}
 
-        output += "\n"
-    }
+		output += "\n"
+	}
 
-    return
+	return
 }
 
 func (game *Game) transformCellToSymbol(cell int) (symbol string) {
-    cellTypes := map[int]string{
-        CELL_TYPE_FOOD:     "o",
-        CELL_TYPE_OBSTACLE: "#",
-    }
+	cellTypes := map[int]string{
+		CELL_TYPE_FOOD:     "o",
+		CELL_TYPE_OBSTACLE: "#",
+	}
 
-    symbol = cellTypes[cell]
+	symbol = cellTypes[cell]
 
-    if symbol == "" {
-        symbol = "."
-    }
+	if symbol == "" {
+		symbol = "."
+	}
 
-    return symbol
+	return symbol
 }
