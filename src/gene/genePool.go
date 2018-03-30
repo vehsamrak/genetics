f624caea1c2b9bc9e2619fa63933ef2f1ec2a871
Petr Karmashev <smonkl@bk.ru>
2018-03-31 02:08:26 +0300

    Gene pool documentation

--- src/gene/genePool.go
+++ src/gene/genePool.go
@@ -1,10 +1,12 @@
 package gene
 
+// GenePool is gene collection
 type GenePool struct {
 	cursor int
 	genes  []gene
 }
 
+// Next moves cursor to next gene in gene pool
 func (genePool *GenePool) Next() {
 	genePoolLength := len(genePool.genes)
 
@@ -19,10 +21,12 @@ func (genePool *GenePool) Next() {
 	}
 }
 
+// Add gene to gene pool
 func (genePool *GenePool) Add(gene gene) {
 	genePool.genes = append(genePool.genes, gene)
 }
 
+// ExecuteCurrentGene runs action of the gene that is currently pointed by cursor
 func (genePool *GenePool) ExecuteCurrentGene() (ok bool, err error) {
 	gene := genePool.genes[genePool.cursor]
 	ok, err = gene.Act()
@@ -30,6 +34,7 @@ func (genePool *GenePool) ExecuteCurrentGene() (ok bool, err error) {
 	return
 }
 
+// CountGenes for counting all genes in pool
 func (genePool *GenePool) CountGenes() int {
 	return len(genePool.genes)
 }
