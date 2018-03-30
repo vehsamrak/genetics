08798ac47e0c862bd027689eb2a6e96adb1b2496
Petr Karmashev <smonkl@bk.ru>
2018-03-31 02:05:22 +0300

    Gene types enum updated

--- src/gene/geneTypeEnum.go
+++ src/gene/geneTypeEnum.go
@@ -4,12 +4,19 @@ package gene
 type GeneType int
 
 const (
+	// GeneTypePhotosynthesize: gene for photosynthesis
 	GeneTypePhotosynthesize GeneType = iota
+	// GeneTypeEat: gene for eating
 	GeneTypeEat
+	// GeneTypeMoveNorth: gene for moving north
 	GeneTypeMoveNorth
+	// GeneTypeMoveEast: gene for moving east
 	GeneTypeMoveEast
+	// GeneTypeMoveSouth: gene for moving south
 	GeneTypeMoveSouth
+	// GeneTypeMoveWest: gene for moving west
 	GeneTypeMoveWest
+	// GeneTypeWait: gene for standing still
 	GeneTypeWait
 	genesCount
 )
