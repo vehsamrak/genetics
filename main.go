690acb0a2ef51bea9b3915593dfc96798996f937
Petr Karmashev <smonkl@bk.ru>
2018-03-31 01:58:38 +0300

    Graphical random game field

--- main.go
+++ main.go
@@ -1,19 +1,66 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"math/rand"
-	"strconv"
+	"os"
 	"time"
 
-	Bacterium "github.com/vehsamrak/genetics/src/bacterium"
+	"github.com/hajimehoshi/ebiten"
 )
+
+const screenWidth = 10
+const screenHeight = 10
+const speed = 1
+
+var worldCounter int
+var pixels = make([]byte, screenWidth*screenHeight*4)
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
 func main() {
-	bacterium := Bacterium.New()
-	fmt.Println(strconv.Itoa(bacterium.LifePoints()))
+	if err := ebiten.Run(update, screenWidth, screenHeight, 50, "Bacterium simulation"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func update(screen *ebiten.Image) error {
+	if ebiten.IsKeyPressed(ebiten.KeyEscape) {
+		os.Exit(0)
+	}
+
+	if worldCounter > 10-speed {
+		worldCounter = 0
+	} else {
+		worldCounter++
+		screen.ReplacePixels(pixels)
+
+		return nil
+	}
+
+	pixels = make([]byte, screenWidth*screenHeight*4)
+
+	for x := range pixels {
+		// only 4-th pixels are visible, while others are RGB colours
+		if x%4 != 0 {
+			continue
+		}
+
+		if rand.Intn(10) == 0 {
+			pixels[x] = 0xff
+			pixels[x+1] = 0xff
+			pixels[x+2] = 0xff
+			pixels[x+3] = 0xff
+		}
+	}
+
+	screen.ReplacePixels(pixels)
+
+	if ebiten.IsRunningSlowly() {
+		return nil
+	}
+
+	return nil
 }
